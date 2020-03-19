da24843e6219679fab2aa5a173d0bba5f26a689d
semtemp <55463525+semtemp@users.noreply.github.com>
2020-03-19 10:21:02 -0700

    d3 practice

--- client/components/Visualizer.js
+++ client/components/Visualizer.js
@@ -1,10 +1,22 @@
 //traffic view of kubernetes clusters/individual pods
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { select } from 'd3';
+
+const data = [10, 20, 30, 15, 25];
 
 const Visualizer = () => {
+  const svgRef = useRef();
+
+  useEffect(() => {
+    console.log(svgRef);
+    const svg = select(svgRef.current);
+    svg.selectAll('circle').data(data);
+  }, [])
+
   return (
-    <div className="visContainer">
+    <div className='visContainer'>
       <h1>Pod Visualizer</h1>
+      <svg ref={svgRef}></svg>
     </div>
   )
 };
