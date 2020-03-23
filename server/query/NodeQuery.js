76494579ae9f5d4ae177bfc16a3caf2c27d09bd0
Evan Amoranto <evanamoranto@yahoo.com>
2020-03-23 11:47:01 -0700

    DS store and NodeQuery index change

--- server/query/NodeQuery.js
+++ server/query/NodeQuery.js
@@ -1,6 +1,6 @@
 function NodeQuery(data) {
-  (this.name = data.body.items[0].metadata.name),
-  (this.cpu = data.body.items[0].status.allocatable.cpu)
+  (this.name = data.body.items[2].metadata.name),
+  (this.cpu = data.body.items[2].status.allocatable.cpu)
 }
 
 module.exports = { NodeQuery };
