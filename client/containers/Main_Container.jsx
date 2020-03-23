6ac29e702ce803d3d72334c644030a534e16c04b
Kritika Sah <sah.kritika@gmail.com>
2020-03-23 14:09:50 -0700

    added Node component to main container and made styling changes

--- client/containers/Main_Container.jsx
+++ client/containers/Main_Container.jsx
@@ -3,6 +3,7 @@ import Visualizer from '../components/Visualizer';
 import Alerts from '../components/Alerts';
 import TestPod from '../components/TestPod';
 import Pods from '../components/Pods';
+import Nodes from '../components/Nodes';
 
 const Main_Container = () => {
   return (
@@ -10,6 +11,7 @@ const Main_Container = () => {
       <div className='router'></div>
       {/* <TestPod /> */}
       <Pods />
+      <Nodes />
     </div>
   );
 };
