fb807dc2c52e0e0051328adaae3bfccc847f6329
Clara <clarayhkim96@gmail.com>
2020-03-24 11:05:44 -0700

    added new logo and .DS_Store into gitignore file

--- client/containers/Dashboard.jsx
+++ client/containers/Dashboard.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
-import logo from '../logo.png';
+// import logo from '../logo.png';
+// import ThermaKubeLogo from '../ThermakubeLogo.png';
+import icon from '../icon.png';
 
 const Dashboard = props => {
-
   return (
-	<div className="dashboard">
-	  <img src={logo} alt="Logo" />
-	  <Nav defaultActiveKey="" className="flex-column" id="navbar">
-	 	<Nav.Link href="/" className="dashMain">Pods</Nav.Link>
-		<Nav.Link href="/visualizer" className="dashPods">View</Nav.Link>
-		<Nav.Link href="/alerts" className="dashAlerts">Alerts</Nav.Link>
-	  </Nav>
-	</div>
-
+    <div className='dashboard'>
+      {/* <img src={logo} alt='Logo' /> */}
+      <img src={icon} alt='Logo' className='logo' />
+      <Nav defaultActiveKey='' className='flex-column' id='navbar'>
+        <Nav.Link href='/' className='dashMain'>
+          Pods
+        </Nav.Link>
+        <Nav.Link href='/visualizer' className='dashPods'>
+          View
+        </Nav.Link>
+        <Nav.Link href='/alerts' className='dashAlerts'>
+          Alerts
+        </Nav.Link>
+      </Nav>
+    </div>
   );
 };
 
