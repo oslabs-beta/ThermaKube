342373c85b3a633b6995283ca920d20ba51b48fc
Clara <clarayhkim96@gmail.com>
2020-04-03 16:37:39 -0700

    added aws logo

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import awsLogo from '../assets/awsLogo.png';
 // login page gives the option to authenticate AWS credentials or use current-context
 const Login = () => {
   return (
     <div className='loginPage'>
       <div className='loginContainer'>
-        <h4 className='loginTitle'>Login</h4>
+        {/* <h4 className='loginTitle'>Login</h4> */}
+        <img src={awsLogo} className='awsLogo' />
         <form className='loginForm'>
           {/* <h5>Login with AWS</h5> */}
           <label className='inputName'>
