94eedb940314d32b8f2c27fc92f23c6232b263c9
Clara <clarayhkim96@gmail.com>
2020-04-03 16:28:14 -0700

    added login form, need to add functionality for aws sign in, works for current context

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+// login page gives the option to authenticate AWS credentials or use current-context
 const Login = () => {
   return (
-    <div className='loginContainer'>
-      <h4 className='loginTitle'>Login</h4>
+    <div className='loginPage'>
+      <div className='loginContainer'>
+        <h4 className='loginTitle'>Login</h4>
+        <form className='loginForm'>
+          {/* <h5>Login with AWS</h5> */}
+          <label className='inputName'>
+            Access Key ID
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type='text' />
+          </label>
+          <br />
+          <label className='inputName'>
+            Secret Access Key &nbsp;&nbsp;&nbsp;&nbsp;
+            <input type='text' />
+          </label>
+          <br />
+          <button>Sign In with AWS</button>
+        </form>
+        <h6>OR</h6>
+        <br />
+        <Link to='/cluster' className='contextLink'>
+          Use Configured Current Context
+        </Link>
+      </div>
     </div>
   );
 };
