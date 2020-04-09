e1f88b28e8767b4be16147fb9f2557ff07540211
Clara <clarayhkim96@gmail.com>
2020-04-09 03:29:57 -0700

    added ui effects for landing page

--- client/components/home/Features.jsx
+++ client/components/home/Features.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-//screenshots
+import Fade from 'react-reveal/Fade';
+// screenshots
 import cluster_ss from '../../assets/cluster_screenshot.png';
 import vis_ss from '../../assets/vis_screenshot.png';
 
@@ -8,32 +9,36 @@ const Features = () => {
     <section id='features' className='featuresContainer'>
       <h2>Visualize Your Kubernetes Clusters</h2>
       <div className='featureInfo'>
-        <section className='featureImgs'>
-          <img src={cluster_ss} />
-          <img src={vis_ss} />
-        </section>
-        <section className='featureDesc'>
-          <h4>Monitor and Visualize your Kubernetes Clusters</h4>
-          <p className='featureText'>
-            ThermaKube monitors the health and performance of Kubernetes
-            clusters with support for Amazon's Elastic Kubernetes Service (EKS)
-            deployments. It tracks real-time data and renders visualization of
-            clusters, and alerts you when pods within the cluster crash - all
-            features you can utilize with a click of a button, without having to
-            download or configure anything!
-          </p>
-          <h4>Why Use Us</h4>
-          <p className='featureText'>
-            We provide:
-            <li>a simplified way to display your Kubernetes cluster data</li>
-            <li>
-              a visualization tool to display relationships within the cluster,
-              as well as usage data on pods
-            </li>
-            <br />
-            <i>Monitor your pods with a click of a button!</i>
-          </p>
-        </section>
+        <Fade left>
+          <section className='featureImgs'>
+            <img src={cluster_ss} />
+            <img src={vis_ss} />
+          </section>
+        </Fade>
+        <Fade right>
+          <section className='featureDesc'>
+            <h4>Monitor and Visualize your Kubernetes Clusters</h4>
+            <p className='featureText'>
+              ThermaKube monitors the health and performance of Kubernetes
+              clusters with support for Amazon's Elastic Kubernetes Service
+              (EKS) deployments. It tracks real-time data and renders
+              visualization of clusters, and alerts you when pods within the
+              cluster crash - all features you can utilize with a click of a
+              button, without having to download or configure anything!
+            </p>
+            <h4>Why Use Us</h4>
+            <p className='featureText'>
+              We provide:
+              <li>a simplified way to display your Kubernetes cluster data</li>
+              <li>
+                a visualization tool to display relationships within the
+                cluster, as well as usage data on pods
+              </li>
+              <br />
+              <i>Monitor your pods with a click of a button!</i>
+            </p>
+          </section>
+        </Fade>
       </div>
     </section>
   );
--- client/components/home/Hero.jsx
+++ client/components/home/Hero.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
+import FadeIn from 'react-fade-in';
 import icon from '../../assets/icon.png';
 import GitHubButton from 'react-github-btn';
+import { Fade } from 'react-bootstrap';
 
 const Hero = () => {
   return (
     <section className='heroContainer'>
       <div className='heroComp'>
-        <div className='col1'>
-          <img src={icon} alt='Logo' className='logoMain' />
-        </div>
-        <h1 className='logoName'>ThermaKube</h1>
-        <div className='github-buttons'>
-          <GitHubButton
-            href='https://github.com/oslabs-beta/ThermaKube'
-            data-size='large'
-            data-show-count='true'
-          >
-            Follow ThermaKube
-          </GitHubButton>
-        </div>
+        <FadeIn delay='500' transitionDuration='1000'>
+          <h3 className='heroQuote'>
+            Power your metrics and monitoring with a modern open-source solution
+            for <br /> any Kubernetes cluster
+          </h3>
+          <div className='col1'>
+            <img src={icon} alt='Logo' className='logoMain' />
+          </div>
+          <h1 className='logoName'>ThermaKube</h1>
+          <div className='github-buttons'>
+            <GitHubButton
+              href='https://github.com/oslabs-beta/ThermaKube'
+              data-size='large'
+              data-show-count='true'
+            >
+              Follow ThermaKube
+            </GitHubButton>
+          </div>
+        </FadeIn>
       </div>
     </section>
   );
