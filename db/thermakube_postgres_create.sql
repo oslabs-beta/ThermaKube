eed4f8615635a61cd4816bf4d68cfb2eb9aa8f67
Clara <clarayhkim96@gmail.com>
2020-04-10 18:41:41 -0700

    create sql tables

---
+++ db/thermakube_postgres_create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE "users" (
+    "_id" serial NOT NULL,
+    "name" varchar(255) NOT NULL,
+    "email" varchar(255) NOT NULL,
+    "password" varchar(255) NOT NULL
+)
+
+CREATE TABLE "awsUsers" (
+    "_id" serial NOT NULL,
+    "access_key_id" varchar(255) NOT NULL,
+    "secret_access_key" varchar(255) NOT NULL,
+)
+
+CREATE TABLE "alerts" (
+    "_id" serial NOT NULL,
+    "pod_name" varchar(255) NOT NULL,
+    "namespace" varchar(255) NOT NULL,
+    "status" varchar(255) NOT NULL,
+    "pod_ip" varchar(255),
+    "timestamp" TIMESTAMP NOT NULL
+)
